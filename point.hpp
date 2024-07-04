8f01dbf03d6404fdfeff4cb6327a4240cfd17286
Ebishu-0309 <shue0309@gmail.com>
2024-07-05 00:53:10 +0900

    trifle

--- point.hpp
+++ point.hpp
@@ -1,8 +1,8 @@
 struct Point {
-    lint x, y;
+    long long x, y;
 
     Point() = default;
-    constexpr Point(lint x_, lint y_) : x(x_), y(y_) {}
+    constexpr Point(long long x_, long long y_) : x(x_), y(y_) {}
 
     constexpr bool operator==(const Point &p) const { return x == p.x && y == p.y; }
     constexpr bool operator<(const Point &p) const {
@@ -14,7 +14,7 @@ struct Point {
     constexpr Point operator-() const { return {-x, -y}; }
     constexpr Point operator+(const Point &p) const { return {x + p.x, y + p.y}; }
     constexpr Point operator-(const Point &p) const { return {x - p.x, y - p.y}; }
-    constexpr Point operator*(lint s) const { return {x * s, y * s}; }
+    constexpr Point operator*(long long s) const { return {x * s, y * s}; }
     constexpr Point &operator+=(const Point &p) {
         x += p.x;
         y += p.y;
@@ -26,15 +26,15 @@ struct Point {
         return *this;
     }
 
-    constexpr lint dot(const Point &p) const { return x * p.x + y * p.y; }
-    constexpr lint cross(const Point &p) const { return x * p.y - y * p.x; }
+    constexpr long long dot(const Point &p) const { return x * p.x + y * p.y; }
+    constexpr long long cross(const Point &p) const { return x * p.y - y * p.x; }
 
-    constexpr lint lengthSq() const { return x * x + y * y; }
+    constexpr long long lengthSq() const { return x * x + y * y; }
     double length() const { return hypot(x, y); }
-    constexpr lint distanceSq(const Point &p) const { return (*this - p).lengthSq(); }
+    constexpr long long distanceSq(const Point &p) const { return (*this - p).lengthSq(); }
     double distance(const Point &p) const { return (*this - p).length(); }
 
-    friend constexpr Point operator*(lint s, const Point &p) { return p * s; }
+    friend constexpr Point operator*(long long s, const Point &p) { return p * s; }
     friend istream &operator>>(istream &is, Point &p) { return is >> p.x >> p.y; }
     friend ostream &operator<<(ostream &os, const Point &p) { return os << "(" << p.x << "," << p.y << ")"; }
 };
