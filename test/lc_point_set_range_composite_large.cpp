c7447ff4aceabf20a0099ea1a7aad58b3caa2e09
Ebishu-0309 <shue0309@gmail.com>
2025-02-01 18:27:29 +0900

    Point Set Range Composite (Large Array)

---
+++ test/lc_point_set_range_composite_large.cpp
@@ -0,0 +1,30 @@
+// competitive-verifier: PROBLEM https://judge.yosupo.jp/problem/point_set_range_composite_large_array
+
+#include "../template.hpp"
+
+#include "../dynamic_segtree.hpp"
+#include "../linear_function.hpp"
+
+using S = LinearFunction<mint>;
+S op(S f, S g) {  // g(f(x))
+    return g(f);
+}
+int main() {
+    int n, q;
+    cin >> n >> q;
+    DynamicSegtree<S, op, S::id> seg(0, n);
+    while (q--) {
+        int t;
+        cin >> t;
+        if (t == 0) {
+            int p;
+            S f;
+            cin >> p >> f;
+            seg.set(p, f);
+        } else {
+            int l, r, x;
+            cin >> l >> r >> x;
+            cout << seg.prod(l, r).calc(x) << "\n";
+        }
+    }
+}
